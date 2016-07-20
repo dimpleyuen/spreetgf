4ac5428449c5e61b84a9784f07f49b4399dd0cc3
Dimple Yuen <dimpleyuen@gmail.com>
2016-07-20 13:41:06 +0800

    feat(product): amend party package 10 pax show page

--- vendor/bundle/ruby/2.2.0/gems/spree_core-3.1.0/app/models/spree/image.rb
+++ vendor/bundle/ruby/2.2.0/gems/spree_core-3.1.0/app/models/spree/image.rb
@@ -3,7 +3,7 @@ module Spree
     validate :no_attachment_errors
 
     has_attached_file :attachment,
-                      styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' },
+                      styles: { mini: '48x48>', small: '100x100>', product: '500x500>', large: '600x600>' },
                       default_style: :product,
                       url: '/spree/products/:id/:style/:basename.:extension',
                       path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
