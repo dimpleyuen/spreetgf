28299f6364dee6bc0b5e136fa58c08fffc042840
Dimple Yuen <dimpleyuen@gmail.com>
2016-08-10 17:11:28 +0800

    feat(products): copyrighting on homepage ; products page layout

--- app/controllers/spree/shop_controller.rb
+++ app/controllers/spree/shop_controller.rb
@@ -6,7 +6,7 @@ module Spree
     def index
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products.includes(:possible_promotions)
-      @products = @products.includes(:taxons).order("spree_taxons.name asc")
+      @products = @products.includes(:taxons).order("spree_taxons.name desc").order("spree_products.name asc")
       @taxonomies = Spree::Taxonomy.includes(root: :children)
     end
   end
